d7983588367061b996097ce75ceb456c7f653de3
email <2879393299@qq.com>
2018-11-27 14:35:41 +0800

    dierci

--- app/src/main/java/com/example/ceshi1/MainActivity.java
+++ app/src/main/java/com/example/ceshi1/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private File tempFile;
     //相册请求码git
     //大大大大555
+    //天同
     private static final int ALBUM_REQUEST_CODE = 1;
     //相机请求码
     private static final int CAMERA_REQUEST_CODE = 2;
